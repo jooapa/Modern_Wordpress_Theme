9f53054d375c4ef26e80d73b92e7078fd5ebdc1d
jooapa <37744313+jooapa@users.noreply.github.com>
2023-02-22 11:00:37 +0200

    wp v 1

---
+++ wp_teema/footer.php
@@ -0,0 +1,12 @@
+	</div>
+</div>
+<div id="footer">
+	<p class="widthlimiter">
+		Copyright &copy; <a href="<?php bloginfo('url'); ?>/"><?php bloginfo('name'); ?></a>. Design by <a href="https://www.jooa.pa/">Jooa</a>.
+	</p>
+</div>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+</body>
+</html>
+
+
---
+++ wp_teema/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php wp_title(' '); if(wp_title(' ', false)) { echo ' | '; } bloginfo('name'); ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+  <script language="JavaScript" src="<?php bloginfo('template_url'); ?>/teemanjavascript.js"></script>
+	<?php wp_head(); ?>
+</head>
+<body>
+<div id="header">
+	<div class="widthlimiter">
+		<div>
+			<span id="description">
+				<?php bloginfo('description'); ?>
+			</span>
+			<form id="search" method="get" action="<?php bloginfo('siteurl')?>/">
+				<input type="text" name="s" id="searchbox" class="textbox" value="<?php echo wp_specialchars($s, 1); ?>" />
+				<input id="btnSearch" type="submit" name="submit" value="<?php _e('Search'); ?>" />
+			</form>
+		</div>
+		<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+	</div>
+</div>
+
+
+<div id="menu">
+	<div class="widthlimiter">
+		<ul id="hj_NavId">
+			<?php wp_list_pages('title_li=')?>
+		</ul>
+	</div>
+</div>
+
+<div id="content">
+	<div class="widthlimiter">
